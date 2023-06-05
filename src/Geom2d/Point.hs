f8d1ecd933ef085d43ed44d29884ce554b468aa4
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-05 18:32:54 +0500

    Displacement operation

--- src/Geom2d/Point.hs
+++ src/Geom2d/Point.hs
@@ -33,3 +33,11 @@ Vector x1 y1 ^+^ Vector x2 y2 = Vector (x1 + y1) (x2 + y2)
 
 (^-^) :: Vector -> Vector -> Vector
 Vector x1 y1 ^-^ Vector x2 y2 = Vector (x1 - y1) (x2 - y2)
+
+scaledBy :: R -> Vector -> Vector
+scaledBy factor (Vector u v) = Vector (factor * u) (factor * v)
+
+displaced :: Point -> Vector -> R -> Point
+displaced (Point x y) vector times =
+  let scaledVec = scaledBy times vector
+   in Point (x + scaledVec.u) (y + scaledVec.v)
