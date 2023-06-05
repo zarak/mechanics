5da56cc5b2581a966147fd78a607f90f9654e7f0
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-05 18:59:11 +0500

    Implement length function

--- src/Geom2d/Vector.hs
+++ src/Geom2d/Vector.hs
@@ -5,6 +5,7 @@ module Geom2d.Vector where
 
 import Geom2d.Nums
 import Geom2d.Point
+import Prelude hiding (length)
 
 data Vector = Vector {u :: R, v :: R}
   deriving (Show)
@@ -21,7 +22,20 @@ Vector x1 y1 ^-^ Vector x2 y2 = Vector (x1 - y1) (x2 - y2)
 scaledBy :: R -> Vector -> Vector
 scaledBy factor (Vector u v) = Vector (factor * u) (factor * v)
 
-displaced :: Point -> Vector -> R -> Point
-displaced (Point x y) vector times =
+displaced :: R -> Vector -> Point -> Point
+displaced times vector (Point x y) =
   let scaledVec = scaledBy times vector
    in Point (x + scaledVec.u) (y + scaledVec.v)
+
+-- Properties
+norm :: Vector -> R
+norm vector = sqrt (vector.u ** 2 + vector.v ** 2)
+
+isNormal :: Vector -> Bool
+isNormal = isCloseToOne . norm
+
+normalized :: Vector -> Vector
+normalized vector = scaledBy (1.0 / norm vector) vector
+
+withLength :: R -> Vector -> Vector
+withLength length = scaledBy length . normalized
