ca80419aefde683ce60105439082ebed9f550c62
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-06 06:57:40 +0500

    Add a property test for commutativity of addition

--- test/Geom2d/VectorSpec.hs
+++ test/Geom2d/VectorSpec.hs
@@ -3,13 +3,19 @@ module Geom2d.VectorSpec (spec) where
 -- import Geom2d.Nums (areCloseEnough)
 import Geom2d.Vector
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.Invariant
+import Test.QuickCheck
 
--- import Test.Hspec.QuickCheck
--- import Test.Invariant
--- import Test.QuickCheck
+newtype VectorWrapper = VectorWrapper Vector deriving (Eq, Show)
 
--- instance Arbitrary Vector where
---   arbitrary = genVector
+instance Arbitrary VectorWrapper where
+  arbitrary = do
+    u <- arbitrary
+    VectorWrapper . Vector u <$> arbitrary
+
+commutativeWrapper :: VectorWrapper -> VectorWrapper -> Bool
+commutativeWrapper (VectorWrapper v1) (VectorWrapper v2) = commutative (^+^) v1 v2
 
 spec :: Spec
 spec = do
@@ -20,5 +26,5 @@ spec = do
           expected = Vector 5 8
       u ^+^ v `shouldBe` expected
 
--- prop "is commutative" $ do
---   commutative (^+^)
+  prop "is commutative" $ do
+    commutativeWrapper
