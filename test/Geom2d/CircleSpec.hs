ef434850ef4a106da00b499aca582c55119bd140
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-07 15:16:52 +0500

    Pass a test for toPolygon

--- test/Geom2d/CircleSpec.hs
+++ test/Geom2d/CircleSpec.hs
@@ -2,6 +2,7 @@ module Geom2d.CircleSpec (spec) where
 
 import Geom2d.Circle
 import Geom2d.Point (Point (..))
+import Geom2d.Polygon (mkPolygon)
 import Test.Hspec
 
 spec :: Spec
@@ -22,3 +23,7 @@ spec = do
     it "computes the circumference of a circle of radius 2" $ do
       let expected = 4 * pi
       circumference circle2 `shouldBe` expected
+  describe "toPolygon" $ do
+    it "gives a polygon approximation to a given circle" $ do
+      let expected = mkPolygon [Point 1 0, Point 0 1, Point (-1) 0, Point 0 (-1)]
+      toPolygon circle1 4 `shouldBe` expected
