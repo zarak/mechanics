aa38e6452eef7d18467649878b221aee5bc8845a
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-07 10:13:13 +0500

    Implement line factories

--- src/Geom2d/Line.hs
+++ src/Geom2d/Line.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Geom2d.Line where
 
 import Geom2d.Point (Point)
-import Geom2d.Vector (Vector)
+import Geom2d.Vector (Vector, perpendicular)
 import Geom2d.Vector qualified as Vector (isParallelTo, isPerpendicularTo)
 
 data Line = Line
@@ -15,3 +17,11 @@ isParallelTo l1 l2 = direction l1 `Vector.isParallelTo` direction l2
 
 isPerpendicularTo :: Line -> Line -> Bool
 isPerpendicularTo l1 l2 = direction l1 `Vector.isPerpendicularTo` direction l2
+
+perpendicularThrough :: Point -> Line -> Line
+perpendicularThrough p line =
+  Line p (perpendicular line.direction)
+
+parallelThrough :: Point -> Line -> Line
+parallelThrough p line =
+  Line p line.direction
