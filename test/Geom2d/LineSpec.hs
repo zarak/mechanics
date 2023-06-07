654ff7ee7b81259161e7dd647a511f67093a7c05
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-07 10:34:02 +0500

    Pass tests for intersectionWith

---
+++ test/Geom2d/LineSpec.hs
@@ -0,0 +1,19 @@
+module Geom2d.LineSpec (spec) where
+
+import Geom2d.Line
+import Geom2d.Point (Point (..))
+import Geom2d.Vector (Vector (..))
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "intersectionWith" $ do
+    it "returns Nothing if the lines are parallel" $ do
+      let l1 = Line (Point 0 0) (Vector 1 1)
+          l2 = Line (Point 10 10) (Vector 1 1)
+      l1 `intersectionWith` l2 `shouldBe` Nothing
+    it "returns the point of intersection of nonparallel lines" $ do
+      let l1 = Line (Point 50 0) (Vector 0 1)
+          l2 = Line (Point 0 30) (Vector 1 0)
+          expected = Point 50 30
+      l1 `intersectionWith` l2 `shouldBe` pure expected
