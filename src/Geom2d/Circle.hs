5d4e04ebe40543ed0813d836f823de86e0cddb7d
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-07 15:12:24 +0500

    Implement toPolygon

--- src/Geom2d/Circle.hs
+++ src/Geom2d/Circle.hs
@@ -2,8 +2,9 @@
 
 module Geom2d.Circle where
 
-import Geom2d.Nums (R)
+import Geom2d.Nums (R, areCloseEnough)
 import Geom2d.Point
+import Geom2d.Polygon (Polygon, mkPolygon)
 
 data Circle = Circle
   { center :: Point,
@@ -11,8 +12,25 @@ data Circle = Circle
   }
   deriving (Show)
 
+instance Eq Circle where
+  c1 == c2 =
+    c1.center == c2.center
+      && areCloseEnough 1e-10 c1.radius c2.radius
+
 area :: Circle -> R
 area circle = pi * circle.radius ** 2
 
 circumference :: Circle -> R
 circumference circle = 2 * pi * circle.radius
+
+containsPoint :: Circle -> Point -> Bool
+containsPoint circle p = circle.center `distanceTo` p < circle.radius
+
+toPolygon :: Circle -> R -> Maybe Polygon
+toPolygon circle divisions =
+  let angleDelta = 2 * pi / divisions
+      pointAtAngle c angle =
+        Point
+          (c.center.x + c.radius * cos angle)
+          (c.center.y + c.radius * sin angle)
+   in mkPolygon [pointAtAngle circle (angleDelta * i) | i <- [0 .. divisions - 1]]
