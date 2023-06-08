8ac86a9f1da4977d911fd0fae552e0f7b218ca55
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-08 20:24:07 +0500

    Pass apply to segment test

--- test/Geom2d/AffineTransfSpec.hs
+++ test/Geom2d/AffineTransfSpec.hs
@@ -1,12 +1,14 @@
 module Geom2d.AffineTransfSpec where
 
-import Geom2d.AffineTransf (AffineTransform (..), applyToPoint, defaultTransf)
+import Geom2d.AffineTransf (AffineTransform (..), applyToPoint, applyToSegment, defaultTransf)
 import Geom2d.Point (Point (..))
+import Geom2d.Segment
 import Test.Hspec
 
 spec :: Spec
 spec = do
   let point = Point 2 3
+      segment = Segment (Point 1 1) (Point 2 2)
       scale = defaultTransf {sx = 2, sy = 5}
       trans = defaultTransf {sx = 1, sy = 1, tx = 10, ty = 15}
       shear = defaultTransf {sx = 1, sy = 1, shx = 3, shy = 4}
@@ -21,3 +23,7 @@ spec = do
     it "shears a point" $ do
       let expected = Point 11 11
       shear `applyToPoint` point `shouldBe` expected
+  describe "applyToSegment" $ do
+    it "scales a segment" $ do
+      let expected = Segment (Point 2 5) (Point 4 10)
+      applyToSegment scale segment `shouldBe` expected
