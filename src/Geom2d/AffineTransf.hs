262aa74f717065ca2d2098a576627d4faf3d7612
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-08 19:02:21 +0500

    Create AffineTransform module

---
+++ src/Geom2d/AffineTransf.hs
@@ -0,0 +1,23 @@
+module AffineTransf where
+
+import Geom2d.Nums (R)
+import Geom2d.Point (Point (..))
+
+data AffineTransform = AffineTransform
+  { sx :: R,
+    sy :: R,
+    tx :: R,
+    ty :: R,
+    shx :: R,
+    shy :: R
+  }
+  deriving (Show)
+
+identity :: AffineTransform
+identity = AffineTransform 1 1 0 0 0 0
+
+applyToPoint :: AffineTransform -> Point -> Point
+applyToPoint affT p =
+  Point
+    ((affT.sx * p.x) + (affT.shx * p.y) + affT.tx)
+    ((affT.shy * p.x) + (affT.sy * p.y) + affT.ty)
