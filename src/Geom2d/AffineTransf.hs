aa050a819da8f8c17798fb45cb704beca42aff23
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-08 20:18:25 +0500

    Transform rects, circles, polygons

--- src/Geom2d/AffineTransf.hs
+++ src/Geom2d/AffineTransf.hs
@@ -1,7 +1,13 @@
 module Geom2d.AffineTransf where
 
+import Geom2d.Circle
+import Geom2d.Circle qualified as Circle (toPolygon)
 import Geom2d.Nums (R)
 import Geom2d.Point (Point (..))
+import Geom2d.Polygon
+import Geom2d.Rect
+import Geom2d.Rect qualified as Rect (toPolygon)
+import Geom2d.Segment
 
 data AffineTransform = AffineTransform
   { sx :: R,
@@ -21,3 +27,21 @@ applyToPoint affT p =
   Point
     ((affT.sx * p.x) + (affT.shx * p.y) + affT.tx)
     ((affT.shy * p.x) + (affT.sy * p.y) + affT.ty)
+
+applyToSegment :: AffineTransform -> Segment -> Segment
+applyToSegment affT (Segment start end) =
+  Segment (applyToPoint affT start) (applyToPoint affT end)
+
+applyToPolygon :: AffineTransform -> Polygon -> Maybe Polygon
+applyToPolygon affT polygon = mkPolygon transformedPoints
+  where
+    transformedPoints = fmap (applyToPoint affT) (vertices polygon)
+
+applyToRect :: AffineTransform -> Rect -> Maybe Polygon
+applyToRect affT rect = Rect.toPolygon rect >>= applyToPolygon affT
+
+applyToCircle :: AffineTransform -> Circle -> R -> Maybe Polygon
+applyToCircle affT circle divisions = Circle.toPolygon circle divisions >>= applyToPolygon affT
+
+applyToCircle30 :: AffineTransform -> Circle -> Maybe Polygon
+applyToCircle30 affT circle = applyToCircle affT circle 30
