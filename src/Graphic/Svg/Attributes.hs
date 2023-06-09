c6940fa58a0ced192dabc4d3519705f49e1577e0
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-09 23:02:49 +0500

    Implement affine transform attribute

---
+++ src/Graphic/Svg/Attributes.hs
@@ -0,0 +1,27 @@
+module Graphic.Svg.Attributes where
+
+import Geom2d.AffineTransf
+import Geom2d.Nums (R)
+
+strokeColor :: String -> String
+strokeColor color = "stroke=\"" <> color <> "\""
+
+strokeWidth :: R -> String
+strokeWidth width = "stroke-width=\"" <> show width <> "\""
+
+fillColor :: String -> String
+fillColor color = "fill=\"" <> color <> "\""
+
+fillOpacity :: R -> String
+fillOpacity opacity = "fill-opacity=\"" <> show opacity <> "\""
+
+affineTransform :: AffineTransform -> String
+affineTransform t =
+  let sxVal = show t.sx
+      shyVal = show t.shy
+      shxVal = show t.shx
+      syVal = show t.sy
+      txVal = show t.tx
+      tyVal = show t.ty
+      values = unwords [sxVal, shyVal, shxVal, syVal, txVal, tyVal]
+   in "transform=\"matrix(" <> values <> ")\""
--- src/Graphic/Svg/Read.hs
+++ src/Graphic/Svg/Read.hs
@@ -1,12 +1,12 @@
 module Graphic.Svg.Read where
 
 import Data.ByteString qualified as BS
-import Data.Text (Text)
+import Data.Text (unpack)
 import Data.Text.Encoding (decodeUtf8)
 import System.Directory (makeAbsolute)
 
-readTemplate :: FilePath -> IO Text
+readTemplate :: FilePath -> IO String
 readTemplate fileName = do
   absolutePath <- makeAbsolute ("src/Graphic/Svg/templates/" <> fileName)
   bytesStr <- BS.readFile absolutePath
-  pure (decodeUtf8 bytesStr)
+  pure $ unpack (decodeUtf8 bytesStr)
