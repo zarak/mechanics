67fabbf87299f5b757932e24bc9302904762dbf2
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-09 20:01:56 +0500

    Implement readTemplate

--- src/Graphic/Svg/Read.hs
+++ src/Graphic/Svg/Read.hs
@@ -1,1 +1,12 @@
 module Graphic.Svg.Read where
+
+import Data.ByteString qualified as BS
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+import System.Directory (makeAbsolute)
+
+readTemplate :: FilePath -> IO Text
+readTemplate fileName = do
+  absolutePath <- makeAbsolute ("templates/" <> fileName)
+  bytesStr <- BS.readFile absolutePath
+  pure (decodeUtf8 bytesStr)
