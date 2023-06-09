940c351b1ff0fd6542cc1b82c1c1f504148de5a2
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-09 17:31:31 +0500

    WIP: organize modules

--- src/Geom2d.hs
+++ src/Geom2d.hs
@@ -1,20 +1,32 @@
-module Geom2d
-  ( Circle,
-    Line,
-  )
-where
-
-import Geom2d.AffineTransf
-import Geom2d.AffineTransforms
-import Geom2d.Circle
-import Geom2d.Interpolation
-import Geom2d.Line
-import Geom2d.Nums
-import Geom2d.OpenInterval
-import Geom2d.Polygon
-import Geom2d.Rect
-import Geom2d.Segment
-import Geom2d.Size
-import Geom2d.TParameter
-import Geom2d.Vector
-import Geom2d.Vectors
+-- module Geom2d
+--   ( module Geom2d.AffineTransf,
+--     module Geom2d.AffineTransforms,
+--     module Geom2d.Circle,
+--     module Geom2d.Interpolation,
+--     module Geom2d.Line,
+--     module Geom2d.Nums,
+--     module Geom2d.OpenInterval,
+--     module Geom2d.Polygon,
+--     module Geom2d.Rect,
+--     module Geom2d.Segment,
+--     module Geom2d.Size,
+--     module Geom2d.TParameter,
+--     module Geom2d.Vector,
+--     module Geom2d.Vectors,
+--   )
+-- where
+--
+-- import Geom2d.AffineTransf
+-- import Geom2d.AffineTransforms
+-- import Geom2d.Circle
+-- import Geom2d.Interpolation
+-- import Geom2d.Line
+-- import Geom2d.Nums
+-- import Geom2d.OpenInterval
+-- import Geom2d.Polygon
+-- import Geom2d.Rect
+-- import Geom2d.Segment
+-- import Geom2d.Size
+-- import Geom2d.TParameter
+-- import Geom2d.Vector
+-- import Geom2d.Vectors
---
+++ src/Geom2d/TParameter/Internal.hs
@@ -0,0 +1,1 @@
+module Geom2d.TParameter.Internal where
