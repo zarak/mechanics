68415ff0799d7d4c4c7951a03dda9131dd24f07d
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-10 00:58:06 +0500

    Pass test for rect primitive

--- test/Graphics/PrimitivesSpec.hs
+++ test/Graphics/PrimitivesSpec.hs
@@ -3,14 +3,21 @@ module Graphics.PrimitivesSpec where
 import Geom2d.Point
 import Geom2d.Rect
 import Geom2d.Segment
+import Geom2d.Size
 import Graphic.Svg.Primitives qualified as Primitives
 import Test.Hspec
 
 spec :: Spec
 spec = do
   let lineTemplate = "<line x1=\"{{x1}}\" y1=\"{{y1}}\" x2=\"{{x2}}\" y2=\"{{y2}}\" {{attrs}}/>"
+      rectTemplate = "<rect x=\"{{x}}\" y=\"{{y}}\" width=\"{{width}}\" height=\"{{height}}\" {{attrs}}/>"
   describe "segment" $ do
     it "should render an SVG segment" $ do
       let segment = Segment (Point 2 3) (Point 4 5)
           expected = "<line x1=\"2.0\" y1=\"3.0\" x2=\"4.0\" y2=\"5.0\" />"
       Primitives.segment segment [] lineTemplate `shouldBe` expected
+  describe "rectangle" $ do
+    it "should render an SVG rectangle" $ do
+      let rect = Rect (Point 2 3) (Size 4 5)
+          expected = "<rect x=\"2.0\" y=\"3.0\" width=\"4.0\" height=\"5.0\" />"
+      Primitives.rectangle rect [] rectTemplate `shouldBe` expected
