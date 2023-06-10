d5c375a4a43c514c5d15ce84d88ee53b79576910
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-10 14:45:53 +0500

    WIP: print parsed results to stdout

--- app/CircleFromPoints/Input.hs
+++ app/CircleFromPoints/Input.hs
@@ -1,8 +1,33 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use <$>" #-}
 module Input where
 
-parsePoints :: IO (String, String, String)
+import Data.Void
+import Geom2d.Nums (R)
+import Geom2d.Point
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer qualified as L (decimal)
+
+pInt :: Parsec Void String R
+pInt = L.decimal
+
+pPoint :: Parsec Void String Point
+pPoint = do
+  x <- pInt
+  space
+  y <- pInt
+  pure (Point x y)
+
+pointFromString :: String -> Either String Point
+pointFromString str = case parse pPoint "" str of
+  Left err -> Left $ errorBundlePretty err
+  Right point -> Right point
+
+parsePoints :: IO (Either String Point, Either String Point, Either String Point)
 parsePoints = do
-  p1 <- getLine
-  p2 <- getLine
-  p3 <- getLine
+  p1 <- pointFromString <$> getLine
+  p2 <- pointFromString <$> getLine
+  p3 <- pointFromString <$> getLine
   pure (p1, p2, p3)
--- app/CircleFromPoints/Main.hs
+++ app/CircleFromPoints/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import Data.Either (fromRight)
 import Input
 
 main :: IO ()
 main = do
   (a, b, c) <- parsePoints
-  putStr $ unlines [a, b, c]
+  let points = fmap (show . fromRight mempty) [a, b, c]
+  putStr $ unlines points
