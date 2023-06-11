191504b6656755fc038779296599816304e1e4e9
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-11 20:15:38 +0500

    Remove debugging traces

--- src/Geom2d/Circles.hs
+++ src/Geom2d/Circles.hs
@@ -1,6 +1,5 @@
 module Geom2d.Circles where
 
-import Debug.Trace (trace)
 import Geom2d.Circle
 import Geom2d.Line
 import Geom2d.Point (Point, distanceTo)
@@ -10,6 +9,6 @@ mkCircleFromPoints :: Point -> Point -> Point -> Maybe Circle
 mkCircleFromPoints p1 p2 p3 = do
   let chordOneBisec = bisector $ Segment p1 p2
       chordTwoBisec = bisector $ Segment p2 p3
-  center <- trace ("bisectors of intersectionWith: " <> show chordOneBisec <> show chordTwoBisec) (chordOneBisec `intersectionWith` chordTwoBisec)
+  center <- chordOneBisec `intersectionWith` chordTwoBisec
   let radius = distanceTo center p1
   pure $ Circle center radius
