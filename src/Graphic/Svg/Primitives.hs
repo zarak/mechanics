d212e935c5db513f3655ae562bea83017fe6834e
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-17 22:34:55 +0500

    Create templates in file

--- src/Graphic/Svg/Primitives.hs
+++ src/Graphic/Svg/Primitives.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Graphic.Svg.Primitives where
 
 import Data.Text (Text)
@@ -11,49 +13,65 @@ import Geom2d.Segment
 import Geom2d.Size
 import Geom2d.Vector
 import Graphic.Svg.Attributes (attrsToStr)
+import Text.RawString.QQ
 
-data Template = Template
-  { _segment :: String,
-    _rectangle :: String,
-    _circle :: String,
-    _polygon :: String,
-    _polyline :: String,
-    _text :: String,
-    _group :: String,
-    _img :: String
-  }
-  deriving (Show)
+lineTemplate :: Text
+lineTemplate = [r|<line x1="{{x1}}" y1="{{y1}}" x2="{{x2}}" y2="{{y2}}" {{attrs}}/>|]
 
-segment :: Segment -> [String] -> String -> String
-segment seg attributes template =
+rectTemplate :: Text
+rectTemplate = [r|<rect x="{{x}}" y="{{y}}" width="{{width}}" height="{{height}}" {{attrs}}/>|]
+
+circleTemplate :: Text
+circleTemplate = [r|<circle cx="{{cx}}" cy="{{cy}}" r="{{r}}" {{attrs}}/>|]
+
+polygonTemplate :: Text
+polygonTemplate = [r|<polygon points="{{points}}" {{attrs}}/>|]
+
+polylineTemplate :: Text
+polylineTemplate = [r|<polyline points="{{points}}" {{attrs}}/>|]
+
+textTemplate :: Text
+textTemplate =
+  [r|<text x="{{x}}" y="{{y}}" dx="{{dx}}" dy="{{dy}}" {{attrs}}> 
+  {{text}} 
+</text>|]
+
+groupTemplate :: Text
+groupTemplate =
+  [r|<g {{attrs}}>
+  {{content}}
+</g>|]
+
+segment :: Segment -> [String] -> String
+segment seg attributes =
   let replacedTemplate =
         T.replace "{{x1}}" (T.pack $ show seg.start.x)
           . T.replace "{{y1}}" (T.pack $ show seg.start.y)
           . T.replace "{{x2}}" (T.pack $ show seg.end.x)
           . T.replace "{{y2}}" (T.pack $ show seg.end.y)
           . T.replace "{{attrs}}" (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ lineTemplate
    in T.unpack replacedTemplate
 
-rectangle :: Rect -> [String] -> String -> String
-rectangle rect attributes template =
+rectangle :: Rect -> [String] -> String
+rectangle rect attributes =
   let replacedTemplate =
         T.replace "{{x}}" (T.pack $ show rect.origin.x)
           . T.replace "{{y}}" (T.pack $ show rect.origin.y)
           . T.replace "{{width}}" (T.pack $ show rect.size.width)
           . T.replace "{{height}}" (T.pack $ show rect.size.height)
           . T.replace "{{attrs}}" (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ rectTemplate
    in T.unpack replacedTemplate
 
-circle :: Circle -> [String] -> String -> String
-circle circ attributes template =
+circle :: Circle -> [String] -> String
+circle circ attributes =
   let replacedTemplate =
         T.replace "{{cx}}" (T.pack $ show circ.center.x)
           . T.replace "{{cy}}" (T.pack $ show circ.center.y)
           . T.replace "{{r}}" (T.pack $ show circ.radius)
           . T.replace "{{attrs}}" (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ circleTemplate
    in T.unpack replacedTemplate
 
 formatPoints :: [Point] -> Text
@@ -62,24 +80,24 @@ formatPoints points =
     " "
     [T.pack (show p.x) <> "," <> T.pack (show p.y) | p <- points]
 
-polygon :: Polygon -> [String] -> String -> String
-polygon pol attributes template =
+polygon :: Polygon -> [String] -> String
+polygon pol attributes =
   let replacedTemplate =
         T.replace "{{points}}" (formatPoints pol.vertices)
           . T.replace "{{attrs}}" (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ polygonTemplate
    in T.unpack replacedTemplate
 
-polyline :: [Point] -> [String] -> String -> String
-polyline points attributes template =
+polyline :: [Point] -> [String] -> String
+polyline points attributes =
   let replacedTemplate =
         T.replace "{{points}}" (formatPoints points)
           . T.replace "{{attrs}}" (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ polylineTemplate
    in T.unpack replacedTemplate
 
-text :: String -> Point -> Vector -> [String] -> String -> String
-text txt pos disp attributes template =
+text :: String -> Point -> Vector -> [String] -> String
+text txt pos disp attributes =
   let replacedTemplate =
         T.replace "{{x}}" (T.pack $ show pos.x)
           . T.replace "{{y}}" (T.pack $ show pos.y)
@@ -87,11 +105,11 @@ text txt pos disp attributes template =
           . T.replace "{{dy}}" (T.pack $ show disp.v)
           . T.replace "{{text}}" (T.pack txt)
           . T.replace "{{attrs}}" (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ textTemplate
    in T.unpack replacedTemplate
 
-group :: [String] -> [String] -> String -> String
-group primitives attributes template =
+group :: [String] -> [String] -> String
+group primitives attributes =
   let replacedTemplate =
         T.replace
           "{{content}}"
@@ -99,11 +117,11 @@ group primitives attributes template =
           . T.replace
             "{{attrs}}"
             (T.pack $ attrsToStr attributes)
-          $ T.pack template
+          $ groupTemplate
    in T.unpack replacedTemplate
 
-arrow :: Segment -> R -> R -> [String] -> Template -> String
-arrow seg arrowLength height attributes templates =
+arrow :: Segment -> R -> R -> [String] -> String
+arrow seg arrowLength height attributes =
   let director = directionVector seg
       -- TODO: refactor withLength order of args so that we can write
       -- arrowLength `withLength` (opposite director)
@@ -114,5 +132,5 @@ arrow seg arrowLength height attributes templates =
       p1 = displaced 1 (v_l ^+^ v_h1) seg.end
       p2 = seg.end
       p3 = displaced 1 (v_l ^+^ v_h2) seg.end
-      poly = polyline [p1, p2, p3] [] templates._polyline
-   in group [segment seg [] templates._segment, poly] attributes templates._group
+      poly = polyline [p1, p2, p3] []
+   in group [segment seg [], poly] attributes
